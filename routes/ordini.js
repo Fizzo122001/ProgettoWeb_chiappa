5c0a56ffde7a7fabee07df81df06b2443a2508a3
Fizzo122001 <chiappamarco3@gmail.com>
2024-05-26 00:10:45 +0200

    fixed page bug tomorrow i will fix ordini

--- routes/ordini.js
+++ routes/ordini.js
@@ -3,19 +3,17 @@ const router = express.Router();
 const DataBase = require("../models/db");
 const db = new DataBase();
 
-
 router.get('/ordini', async (req, res) => {
     if (!req.user)
         return res.redirect("/accedi");
     try {
         const emailUtente = req.user.email;
-        const products = await db.getOrdini(emailUtente);
-        res.render("ordini", { authenticated: req.isAuthenticated(), title: 'Ordini', coach: req.user });
+        const ordini = await db.getOrdini(emailUtente); 
+        res.render("ordini", { authenticated: req.isAuthenticated(), title: 'Ordini', coach: req.user, ordini: ordini }); // Passa gli ordini al template
     } catch (error) {
         console.error('Errore durante la ricerca degli ordini:', error);
         res.status(500).send('Errore durante la ricerca degli ordini.');
     }
 });
-
 
 module.exports = router;
