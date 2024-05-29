a5bc377292071eac3456217c3e4613d828eafd99
Fizzo122001 <chiappamarco3@gmail.com>
2024-05-29 10:27:22 +0200

    Fixed query prenotazione
    Devo solo sistemare la grafica

--- models/db.js
+++ models/db.js
@@ -222,6 +222,36 @@ class DataBase {
         });
     }
 
+    inserisciprenotazione(id_utente, id_servizio ) {
+        const sql = `INSERT INTO Prenotazioni (ID_utente,ID_servizio) VALUES (?, ?)`;
+        return new Promise((resolve, reject) => {
+            this.db.run(sql, [id_utente, id_servizio], function (err) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    }
+
+    controllaPrenotazione(id_utente,id_servizio) {
+        const sql = `SELECT * FROM Prenotazioni WHERE ID_utente = ? AND ID_servizio = ?`;
+        return new Promise((resolve, reject) => {
+            this.db.all(sql, [id_utente,id_servizio], (err, rows) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+        });
+    }
+
+
+
+    
+
 
 
 }
--- routes/servizi_offerti.js
+++ routes/servizi_offerti.js
@@ -12,7 +12,8 @@ router.get('/', async (req, res) => {
     try {
         const servizi = await database.getServizi();
         const coach = req.user.coach;
-        return res.render('servizi_offerti', { authenticated: req.isAuthenticated(), title: 'Servizi Offerti', servizi, coach });
+        const id_utente=req.user.id;
+        return res.render('servizi_offerti', { authenticated: req.isAuthenticated(), title: 'Servizi Offerti', servizi, coach, id_utente });
     } catch (error) {
         console.error(error);
         res.status(500).send('Errore nel recuperare i servizi offerti.');
@@ -21,13 +22,20 @@ router.get('/', async (req, res) => {
 
 router.post('/prenotati', async (req, res) => {
     try {
-        const { ID_servizio } = req.body;
-        await database.posti_disponibili(ID_servizio);
+        const { id_utente, id_servizio } = req.body;
+        const prenotazioni = await database.controllaPrenotazione(id_utente, id_servizio);
+        if (prenotazioni.length > 0) {
+            return res.status(400).send('Sei già prenotato per questo servizio.');
+        }
+
+        await database.posti_disponibili(id_servizio);
+        await database.inserisciprenotazione(id_utente, id_servizio);
         return res.redirect('/servizi_offerti');
     } catch (error) {
-        console.error('Errore durante l\'inserimento della recensione:', error);
-        return res.status(500).send('Errore durante l\'inserimento della recensione.');
+        console.error('Errore durante la prenotazione:', error);
+        return res.status(500).send('Errore durante la prenotazione.');
     }
 });
+
 
 module.exports = router;
